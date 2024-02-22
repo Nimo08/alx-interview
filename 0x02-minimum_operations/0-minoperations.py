7f506309022819b258657760e50a4558ffe8cc6f
Nimo08 <nimomohamed800@gmail.com>
2024-02-22 11:23:45 +0300

    If n <= 1

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,7 +11,7 @@ def minOperations(n):
     Calculates the fewest number of operations needed
     to result in exactly n H characters in the file.
     """
-    if not isinstance(n, int):
+    if n <= 1:
         return 0
     num_op = 0
     # start from 2 'H's
