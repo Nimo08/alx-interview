3ce1ba3f4e6331016930d8a03d2a6c0fb3de335e
Nimo08 <nimomohamed800@gmail.com>
2024-02-22 10:53:20 +0300

    Adding README & scripts

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+In a text file, there is a single character H.
+Your text editor can execute only two operations in this file
+Copy All and Paste.
+"""
+
+
+def minOperations(n: int) -> int:
+    """
+    Calculates the fewest number of operations needed
+    to result in exactly n H characters in the file.
+    """
+    if n == 1:
+        return 0
+    num_op = 0
+    # start from 2 'H's
+    for num in range(2, n + 1):
+        # check if num is a factor of n
+        while n % num == 0:
+            # add number of smaller problems to result
+            num_op += num
+            # create smaller problem: move closer to n == 1
+            n //= num
+    return num_op
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,1 @@
+This directory contains scripts for the tasks in 0x02-minimum_operations.
