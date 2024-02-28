ef95125ff120bb38af734dd49cb37364239e14e1
Nimo08 <nimomohamed800@gmail.com>
2024-02-28 18:46:46 +0300

    Modified script

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,42 +2,35 @@
 """Reads stdin line by line and computes metrics"""
 
 import sys
-import re
 
+status_code_counts = {'200': 0, '301': 0, '400': 0, '401': 0,
+                      '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+line_count = 0
 
-def print_stats(total_size, status_codes):
-    """Prints stats"""
-    print(f"File size: {total_size}")
-    for code in sorted(status_codes.keys()):
-        print(f"{code}: {status_codes[code]}")
+try:
+    for line in sys.stdin:
+        line_parts = line.split(" ")
+        if len(line_parts) > 4:
+            code = line_parts[-2]
+            size = int(line_parts[-1])
+            if code in status_code_counts.keys():
+                status_code_counts[code] += 1
+            total_size += size
+            line_count += 1
 
+        if line_count == 10:
+            line_count = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(status_code_counts.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-def main():
-    """Computes metrics"""
-    total_size = 0
-    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
-                    '403': 0, '404': 0, '405': 0, '500': 0}
-    line_count = 0
+except Exception as e:
+    pass
 
-    try:
-        for line in sys.stdin:
-            match = re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[.*\]\
-                             "GET /projects/260 HTTP/1\.1" (\d{3}) (\d+)$',
-                             line)
-            if match:
-                status_code = match.group(1)
-                if status_code in status_codes:
-                    status_codes[status_code] += 1
-
-                total_size += int(match.group(2))
-                line_count += 1
-
-                if line_count % 10 == 0:
-                    print_stats(total_size, status_codes)
-                    print()
-    except KeyboardInterrupt:
-        print_stats(total_size, status_codes)
-
-
-if __name__ == "__main__":
-    main()
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(status_code_counts.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
