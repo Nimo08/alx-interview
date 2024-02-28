76c77cb22e5ce34e54482daaa1742aafb1b29ab9
Nimo08 <nimomohamed800@gmail.com>
2024-02-28 18:25:36 +0300

    Using print_stats & main methods

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,32 +1,43 @@
 #!/usr/bin/python3
 """Reads stdin line by line and computes metrics"""
+
 import sys
 import re
-from collections import defaultdict
 
-# Initialize counters
-total_file_size = 0
-status_code_counts = defaultdict(int)
 
-try:
-    for line in sys.stdin:
-        # Regex pattern
-        match = re.match(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[.*\] \
-                         "GET /projects/260 HTTP/1.1" (\d+) (\d+)', line)
-        if match:
-            # Extract file size and status code
-            file_size, status_code = int(match.group(3)), int(match.group(2))
-            total_file_size += file_size
-            status_code_counts[status_code] += 1
-            # Stats after every 10 lines
-            if total_file_size % 100 == 0:
-                print(f"File size: {total_file_size}")
-                for code in sorted(status_code_counts.keys()):
-                    print(f"{code}: {status_code_counts[code]}")
-                print()
-except KeyboardInterrupt:
-    # Upon receiving a keyboard interruption
-    print(f"File size: {total_file_size}")
-    for code in sorted(status_code_counts.keys()):
-        print(f"{code}: {status_code_counts[code]}")
-    print()
+def print_stats(total_size, status_codes):
+    """Prints stats"""
+    print(f"File size: {total_size}")
+    for code in sorted(status_codes.keys()):
+        print(f"{code}: {status_codes[code]}")
+
+
+def main():
+    """Computes metrics"""
+    total_size = 0
+    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
+                    '403': 0, '404': 0, '405': 0, '500': 0}
+    line_count = 0
+
+    try:
+        for line in sys.stdin:
+            match = re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[.*\]\
+                             "GET /projects/260 HTTP/1\.1" (\d{3}) (\d+)$',
+                             line)
+            if match:
+                status_code = match.group(1)
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
+
+                total_size += int(match.group(2))
+                line_count += 1
+
+                if line_count % 10 == 0:
+                    print_stats(total_size, status_codes)
+                    print()
+    except KeyboardInterrupt:
+        print_stats(total_size, status_codes)
+
+
+if __name__ == "__main__":
+    main()
