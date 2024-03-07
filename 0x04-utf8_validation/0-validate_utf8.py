85c67729c0be10c4cb366ba66478b9173990a0ef
Nimo08 <nimomohamed800@gmail.com>
2024-03-06 21:01:08 -0800

    Testing windows: vs code push

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,6 +6,7 @@ Determines if a given data set represents a valid UTF-8 encoding.
 
 def validUTF8(data):
     """
+    Use bitwise operators to check through bytes
     Return: True if data is a valid UTF-8 encoding, else return False
     """
     num_bytes = 0
