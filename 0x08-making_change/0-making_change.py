baaa64c6bb8520435b405a7fe976c5aa759691a9
Nimo08 <nimomohamed800@gmail.com>
2024-04-04 12:22:20 +0300

    Modify makeChange

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,16 +12,16 @@ def makeChange(coins, total):
     If total is 0 or less, return 0
     If total cannot be met by any number of coins you have, return -1
     """
-    if total <= 0:
+    if total < 1:
         return 0
-    # except the first element: 0, initialize the small amounts to total + 1
-    # total + 1 is a placeholder
-    dp = [0] + [total + 1] * total
+    coins.sort(reverse=True)
+    coin_count = 0
     for coin in coins:
-        for i in range(coin, total + 1):
-            # find min number of coins to make amount i
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-    if dp[total] != total + 1:
-        return dp[total]
-    else:
-        return -1
+        if total == 0:
+            break
+        # calculates how many times current coin can be used
+        # to make change for remaining total amount
+        current_coin = total // coin
+        total -= current_coin * coin
+        coin_count += current_coin
+    return coin_count if total == 0 else -1
